c4e1ea2f02ed7b60d2027b0fbd76901de954ee61
Eric Jing <jinge9475@gmail.com>
2022-08-28 14:21:11 -0400

    Finished Project

--- src/face_recognition.py
+++ src/face_recognition.py
@@ -2,14 +2,14 @@ import numpy as np
 import cv2 as cv
 import os
 
-haar_cascade = cv.CascadeClassifier("src/haar_face.xml")
+haar_cascade = cv.CascadeClassifier("src/train/haar_face.xml")
 
 people_list = []
 for people in os.listdir("Faces"):
     people_list.append(people)
 
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
-face_recognizer.read("src/face_trained.yml")
+face_recognizer.read("src/train/face_trained.yml")
 
 img = cv.imread(r"Faces/Ben Afflek/9.jpg")
 
@@ -20,7 +20,6 @@ for (x, y, w, h) in faces_rect:
     faces_roi = gray[y:y+h, x:x+h]
 
     label, confidence = face_recognizer.predict(faces_roi)
-    print(f'Label = {people_list[label]} with a confidence of {confidence}')
 
     cv.putText(img, str(people_list[label]), (20, 20), cv.FONT_HERSHEY_COMPLEX, 0.5, (0,0, 255), thickness=2)
     cv.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), thickness=2)
